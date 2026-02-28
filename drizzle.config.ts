bfea5294ae1602af2382e35966be610b104545e2
Jordan Aceves-Foster <jordanaf808@gmail.com>
2026-02-28 12:12:19 -0800

    make sure Drizzle config has all necessary values

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -8,6 +8,12 @@ export default defineConfig({
   schema: './src/db/schema.ts',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    // url: process.env.DATABASE_URL!,
+    host: process.env.DB_HOST!,
+    port: parseInt(process.env.DB_PORT!),
+    user: process.env.DB_USER!,
+    password: process.env.DB_PASSWORD!,
+    database: process.env.DB_NAME!,
+    ssl: false,
   },
 })
